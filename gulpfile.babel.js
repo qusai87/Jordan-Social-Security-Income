626a3f6af829c9ac6d5c7d88aab5821e314b3fba
Qusai Altabbal <qusaitabbal@gmail.com>
2017-07-05 04:58:42 +0300

    fix js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -110,7 +110,10 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
+      './app/scripts/material.min.js',
+      './app/scripts/getmdl-select.min.js',
       './app/scripts/main.js'
+
       // Other scripts
     ])
       .pipe($.newer('.tmp/scripts'))
